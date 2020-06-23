c86521ddd4e2fef6cec56bd93502b5614ea6cb02
idmacleod <idmacleod@gmail.com>
2020-06-23 14:03:00 +0100

    CdDeck has array of CDs, tests pass

--- src/main/java/CdDeck.java
+++ src/main/java/CdDeck.java
@@ -1,3 +1,46 @@
-public class CdDeck {
+import java.util.ArrayList;
+
+public class CdDeck extends Component implements IPlay {
     private boolean playing;
+    private int capacity;
+    private ArrayList<String> cds;
+    
+    public CdDeck(String make, String model, int capacity) {
+        super(make, model);
+        this.capacity = capacity;
+        this.playing = false;
+        this.cds = new ArrayList<String>();
+    }
+
+    public void play() {
+        if (countCds() > 0) {
+            this.playing = true;
+        }
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public void stop() {
+        this.playing = false;
+    }
+
+    public boolean isPlaying() {
+        return this.playing;
+    }
+    
+    public int countCds(){
+        return this.cds.size();
+    }
+    
+    public void addCd(String title) {
+        if(countCds() < this.capacity){
+            this.cds.add(title);
+        }
+    }
+
+    public void removeCd(String title) {
+        this.cds.remove(title);
+    }
 }
--- src/test/java/CdDeckTest.java
+++ src/test/java/CdDeckTest.java
@@ -1,2 +1,73 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+
 public class CdDeckTest {
+    private CdDeck cdDeck;
+
+    @Before
+    public void before() {
+        cdDeck = new CdDeck("Sony", "C800i", 3);
+    }
+
+    @Test
+    public void hasMake() {
+        assertEquals("Sony",cdDeck.getMake());
+    }
+
+    @Test
+    public void hasModel() {
+        assertEquals("C800i", cdDeck.getModel());
+    }
+
+    @Test
+    public void hasCapacity() {
+        assertEquals(3, cdDeck.getCapacity());
+    }
+
+    @Test
+    public void newCdDeckIsNotPlaying() {
+        assertFalse(cdDeck.isPlaying());
+    }
+
+    @Test
+    public void canPlay() {
+        cdDeck.addCd("The White Album");
+        cdDeck.play();
+        assertTrue(cdDeck.isPlaying());
+    }
+
+    @Test
+    public void cannotPlayIfCdDeckEmpty() {
+        cdDeck.play();
+        assertFalse(cdDeck.isPlaying());
+    }
+
+    @Test
+    public void canStop() {
+        cdDeck.play();
+        cdDeck.stop();
+        assertFalse(cdDeck.isPlaying());
+    }
+
+    @Test
+    public void cdsStartsEmpty() {
+        assertEquals(0, cdDeck.countCds());
+    }
+
+    @Test
+    public void canAddCd() {
+        cdDeck.addCd("The White Album");
+        assertEquals(1, cdDeck.countCds());
+    }
+
+    @Test
+    public void canRemoveCd() {
+        cdDeck.addCd("The White Album");
+        cdDeck.addCd("Nevermind");
+        cdDeck.removeCd("The White Album");
+        assertEquals(1, cdDeck.countCds());
+    }
 }
